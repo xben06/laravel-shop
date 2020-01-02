ce3c4d065306ba6eae451c30b917d5d11c861344
xben06 <632173221@qq.com>
2020-01-03 00:52:51 +0800

    邮箱验证

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,8 @@
 */
 
 Route::get('/', 'PagesController@root')->name('root');
+//    ->middleware('verified');;
 
-Auth::routes();
+Auth::routes(['verify'=>true]);
 
 Route::get('/home', 'HomeController@index')->name('home');
