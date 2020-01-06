1e0f1575afb42f9ce928d14a5306a7fbdcbad809
xben06 <632173221@qq.com>
2020-01-07 01:29:54 +0800

    用户界面 - 商品列表

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,8 @@ Route::get('/', 'PagesController@root')->name('root');
 Auth::routes(['verify'=>true]);
 
 Route::get('/home', 'HomeController@index')->name('home');
-
+Route::redirect('/', '/products')->name('root');
+Route::get('products', 'ProductsController@index')->name('products.index');
 
 // auth 中间件代表需要登录，verified中间件代表需要经过邮箱验证
 Route::group(['middleware' => ['auth', 'verified']], function() {
