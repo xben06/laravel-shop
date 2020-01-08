72b67502cd6f213d1266818499b043151e5b08fc
xben06 <632173221@qq.com>
2020-01-09 01:37:14 +0800

    用户界面 - 收藏商品列表

--- routes/web.php
+++ routes/web.php
@@ -19,11 +19,11 @@ Auth::routes(['verify'=>true]);
 Route::get('/home', 'HomeController@index')->name('home');
 Route::redirect('/', '/products')->name('root');
 Route::get('products', 'ProductsController@index')->name('products.index');
+Route::get('products/favorites', 'ProductsController@favorites')->name('products.favorites');
 Route::get('products/{product}', 'ProductsController@show')->name('products.show');
 
 Route::post('products/{product}/favorite', 'ProductsController@favor')->name('products.favor');
 Route::delete('products/{product}/favorite', 'ProductsController@disfavor')->name('products.disfavor');
-
 // auth 中间件代表需要登录，verified中间件代表需要经过邮箱验证
 Route::group(['middleware' => ['auth', 'verified']], function() {
     Route::get('user_addresses', 'UserAddressesController@index')->name('user_addresses.index');
