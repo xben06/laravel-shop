4a1d5bdc70e7e5822ba20a8b9282f1edb58885df
xben06 <632173221@qq.com>
2020-03-18 00:07:36 +0800

    优化优惠券模块

--- app/Models/CouponCode.php
+++ app/Models/CouponCode.php
@@ -5,6 +5,8 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Str;
 use App\Exceptions\CouponCodeUnavailableException;
+use App\Models\User;
+
 class CouponCode extends Model
 {
     // 用常量的方式定义支持的优惠券类型
@@ -61,7 +63,7 @@ class CouponCode extends Model
         return $str.'减'.str_replace('.00', '', $this->value);
     }
 
-    public function checkAvailable($orderAmount = null)
+    public function checkAvailable(User $user,$orderAmount = null)
     {
         if (!$this->enabled) {
             throw new CouponCodeUnavailableException('优惠券不存在');
@@ -81,6 +83,22 @@ class CouponCode extends Model
 
         if (!is_null($orderAmount) && $orderAmount < $this->min_amount) {
             throw new CouponCodeUnavailableException('订单金额不满足该优惠券最低金额');
+        }
+
+        $used = Order::where('user_id', $user->id)
+            ->where('coupon_code_id', $this->id)
+            ->where(function($query) {
+                $query->where(function($query) {
+                    $query->whereNull('paid_at')
+                        ->where('closed', false);
+                })->orWhere(function($query) {
+                    $query->whereNotNull('paid_at')
+                        ->where('refund_status', '!=', Order::REFUND_STATUS_SUCCESS);
+                });
+            })
+            ->exists();
+        if ($used) {
+            throw new CouponCodeUnavailableException('你已经使用过这张优惠券了');
         }
     }
 
@@ -105,4 +123,5 @@ class CouponCode extends Model
             return $this->decrement('used');
         }
     }
+
 }
